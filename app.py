8a7411976fa5a8cfeccf4f7058d1cd7c5f0c4862
Abhishek <abhishek.chauhan@hulu.com>
2021-02-12 00:00:46 -0800

    fix bug

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.from_url(os.environ.get("REDIS_URL"))
+#cache = redis.from_url(os.environ.get("REDIS_URL"))
+#redis://default:FEp8Ddi2Pj73KkIxl01Kz9zRyLhPfkhK@redis-19690.c92.us-east-1-3.ec2.cloud.redislabs.com:19690
+cache = "redis://default:FEp8Ddi2Pj73KkIxl01Kz9zRyLhPfkhK@redis-19690.c92.us-east-1-3.ec2.cloud.redislabs.com:19690"
 
 @app.route('/')
 def get_cache():
-    return cache.get('reddit_job') or "No job data saved."
+    return cache.get('stonks') or "No job data saved."
 
 @app.route('/upload/<data>')
 def set_cache(data):
-    cache.set('reddit_job', data)
+    cache.set('stonks-', data)
     return "Job data saved."
 
 if __name__ == '__main__':
