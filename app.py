0bbe30ade0a24d2b377182c91bd335dbd7ded8f8
Abhishek <abhishek.chauhan@hulu.com>
2021-02-12 00:20:18 -0800

    add cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import redis
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 #cache = redis.from_url(os.environ.get("REDIS_URL"))
 #redis://default:FEp8Ddi2Pj73KkIxl01Kz9zRyLhPfkhK@redis-19690.c92.us-east-1-3.ec2.cloud.redislabs.com:19690
 cache = redis.from_url("redis://default:FEp8Ddi2Pj73KkIxl01Kz9zRyLhPfkhK@redis-19690.c92.us-east-1-3.ec2.cloud.redislabs.com:19690")
