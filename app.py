2a07f93c932e36ad3483574938e82a59e67b11f8
Abhishek <abhishek.chauhan@hulu.com>
2021-02-11 23:41:35 -0800

    add redis

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
+import os
+import redis
 from flask import Flask
+
 app = Flask(__name__)
+cache = redis.from_url(os.environ.get("REDIS_URL"))
 
 @app.route('/')
-def hello_world():
-    return "Sup"
+def get_cache():
+    return cache.get('reddit_job') or "No job data saved."
+
+@app.route('/upload/<data>')
+def set_cache():
+    cache.set("reddit_job", data)
+    return "Job data saved."
 
 if __name__ == '__main__':
     app.run()
